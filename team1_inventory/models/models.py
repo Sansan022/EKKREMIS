e6d932b4cbc10985c8caf7c4ff49aca2f3295998
Nathan <Nathaniel121215@gmail.com>
2022-08-14 16:00:05 +0000

    Update

--- team1_inventory/models/models.py
+++ team1_inventory/models/models.py
@@ -1,6 +1,9 @@
 # -*- coding: utf-8 -*-
 
 from odoo import models, fields, api
+import time
+from odoo.tools import DEFAULT_SERVER_DATETIME_FORMAT
+from datetime import datetime
 
 class team1_inventory(models.Model):
     _inherit = 'product.template'
@@ -8,7 +11,15 @@ class team1_inventory(models.Model):
     description_txt = fields.Text(string="Description:")
 
 class testing(models.Model):
-    _inherit = 'stock.picking.type'
+    _inherit = 'stock.picking'
+
+    min_date = fields.Datetime(compute='_compute_dates', inverse='_set_min_date', store=True,
+        index=True, track_visibility='onchange',default=time.strftime(DEFAULT_SERVER_DATETIME_FORMAT),
+        states={'done': [('readonly', True)], 'cancel': [('readonly', True)]},
+        help="Scheduled time for the first part of the shipment to be processed. Setting manually a value here would set it as expected date for all the stock moves.")
+
+    # datetodayfield = time.strftime(DEFAULT_SERVER_DATETIME_FORMAT)
+    # print(datetodayfield)
 
     # @api.multi
     # def _compute_picking_count(self):
