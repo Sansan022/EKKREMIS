a9f51fb23eacdfdac1b09e2611a012e389fae8b2
benjamin <wsi.odoo.trainees@gmail.com>
2022-08-19 08:41:29 +0000

    team number(not done)

--- team-configuration/models/models.py
+++ team-configuration/models/models.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-from odoo import models, fields, api
+from odoo import models, fields, api, _
 from odoo.exceptions import ValidationError
 
 
@@ -8,43 +8,21 @@ class team_configuration(models.Model):
     _name = 'team.configuration'
     _rec_name = 'sequence'
 
-
     sequence= fields.Char(string='Reference', readonly=True, required=True, copy=False, default='New') 
     team_number = fields.Integer()
     team_members = fields.One2many('team.configuration.line','team_members_lines1')
 
-
     _sql_constraints = [
-    ('name', 'unique (team_number)', 'The team_number already Exists!'),
+    ('team_number', 'UNIQUE(team_number)', 'The team number already Exists!'),
 ]
 
-#    @api.model
-#     def create(self, values):
-#         res = super(ResPartner, self).create(values)
-#         # here you can do accordingly
-#         self.team_number
-#         return res
-
-     
     @api.model
     def create(self, vals):
         # if sequence data is equal to new 
         if vals.get('sequence', 'New') == 'New':
             vals['sequence'] = self.env['ir.sequence'].next_by_code('team_sequence') or 'New'
-            res = super(team_configuration, self).create(vals)
-            return res
+            return super(team_configuration, self).create(vals)
 
-     
-    # @api.onchange('team_members')
-    # def _check_team_members(self):
-    #     x = self.env['team.configuration'].search([]).mapped('team_members')
-    #     for rec in x:
-    #         if rec.team_members_lines.id == self.team_members.team_members_lines.id:
-    #             raise ValidationError("Invalid")
-
-    #     print('x',rec.team_members_lines.name)
-    #     print('member',self.team_members.team_members_lines.name)  
-     
     @api.multi
     @api.onchange('team_members')
     def _check_team_members(self):
@@ -54,27 +32,42 @@ class team_configuration(models.Model):
             s.append(rec.team_members_lines.id)
         try:
             if self.team_members.team_members_lines.id in s:
-                raise ValidationError("Invalid")
+                raise ValidationError("Invalid2")
+                print(s)
+                print(self.team_members.team_members_lines.id )
         except:
-            raise ValidationError("Invalid")
-            
-
+            raise ValidationError("singleton error")
+         
 class team_configuration_line(models.Model):
     _name = 'team.configuration.line'
-    _rec_name = 'team_members_lines'
+    # _rec_name = 'team_members_lines1'
 
     team_members_lines = fields.Many2one('hr.employee')
     team_members_lines1 = fields.Many2one('team.configuration')
-
    
 class team_page(models.Model):
     _inherit = 'hr.employee'
-    _rec_name = 'team_number_lines'
+    # _rec_name = 'team_number_lines'
 
-    team_number_lines = fields.Many2one('team.configuration')
-    team_number_id = fields.Integer(related='team_number_lines.team_number')
+    team_number_id = fields.Integer()
     history = fields.One2many('team.page.lines', 'team_page_lines')
+    samp = fields.Many2one('team.configuration')
 
+    @api.onchange('team_number_id')
+    def _get_number_id(self):
+        # id of employee
+        ids = self._origin
+
+        x = self.env['team.configuration'].search([]).mapped('team_members')
+        y = []
+        for rec in x:
+            y.append(rec.team_members_lines.id)
+            if ids.id in y:
+                print('yes')
+                print(rec.team_number)
+            else:
+                print('no')
+        
 
 class team_page_lines(models.Model):  
     _name = 'team.page.lines'
