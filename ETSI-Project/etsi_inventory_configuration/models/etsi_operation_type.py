301a9e32e898d0e7eaa8e74181ce3d542ae451a0
tranzend-jerald <wsi.odoo.trainees@gmail.com>
2022-08-21 07:47:57 +0000

    ETSI-Inventory-Configuration Updated

---
+++ ETSI-Project/etsi_inventory_configuration/models/etsi_operation_type.py
@@ -0,0 +1,9 @@
+from odoo import api, fields, models
+import time
+from odoo.tools import DEFAULT_SERVER_DATETIME_FORMAT
+from datetime import datetime
+
+
+class stock_picking_inherit(models.Model):
+    _inherit = 'stock.picking.type'
+    subscriber_checkbox = fields.Boolean('Subscriber')
