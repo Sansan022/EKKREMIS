af1cb8bb2dd0b0e7efd8d60595f0dec8e5e58aa4
wsi-odoo-trainees <110818067+wsi-odoo-trainees@users.noreply.github.com>
2022-09-07 15:52:46 +0800

    Delete skycable_employee_inventory directory
    Bad push, luma ang nailagay. Mag iinsert ng backup

--- skycable_employee_inventory/models/skycable_inventory_operation_type.py
+++
@@ -1,9 +0,0 @@
-from odoo import api, fields, models
-import time
-from odoo.tools import DEFAULT_SERVER_DATETIME_FORMAT
-from datetime import datetime
-
-
-class stock_picking_inherit(models.Model):
-    _inherit = 'stock.picking.type'
-    subscriber_checkbox = fields.Boolean('Subscriber')
